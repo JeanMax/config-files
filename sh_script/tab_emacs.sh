464dde3456b09ee8c7d913f3e7683696cb3371e8
JeanMax <mcanal@student.42.fr>
2016-06-30 11:52:58 +0200

    ssh + dired emacs

--- sh_script/tab_emacs.sh
+++ sh_script/tab_emacs.sh
@@ -6,7 +6,7 @@
 #    By: mcanal <zboub@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2015/08/19 13:12:36 by mcanal            #+#    #+#              #
-#    Updated: 2015/09/03 21:59:05 by mcanal           ###   ########.fr        #
+#    Updated: 2016/06/30 11:51:32 by mcanal           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,8 +14,8 @@ if [ -z "$1" ]; then
 	echo "Give me a file, boy...";
 #elif [ ! -f "$1" ]; then
 #	echo "I'm pretty sure you're stoned again...";
-elif [ -d "$1" ]; then
-	echo "Dired mod? Seriouslee? Nah...";
+# elif [ -d "$1" ]; then
+	# echo "Dired mod? Seriouslee? Nah...";
 else	
 	echo -n "\033]0;$(basename "$1")\007";
 	emacs24 -nw $1 $2 $3 $4 $5 $6 $7 $8 $9;
